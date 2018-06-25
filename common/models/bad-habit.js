c9674946be44cf1c9c17abfc182aeeb80904f0ab
Beaule <frederic.lebeau.mobile@gmail.com>
2018-06-25 21:56:22 +0200

    connection (ugly) to API sandbox

--- common/models/bad-habit.js
+++ common/models/bad-habit.js
@@ -65,8 +65,48 @@ module.exports = function(BadHabit) {
 
     //confirm user want to be notify when badhabit happens
     BadHabit.notify= function(product,callback){
-            
             callback(null,"Notifications activated for product: "+product);
+    }; 
+
+     //load Bad habits from API sandbox
+     BadHabit.import= function(callback){
+        var request = require('request');
+        request({
+                headers: { 'Authorization': `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsib2F1dGgyLXJlc291cmNlIl0sInNjb3BlIjpbInNlcnZlciIsInBheW1lbnRzIiwicHJvZmlsZXMiLCJhY2NvdW50cyIsInRyYW5zYWN0aW9ucyIsInBheW1lbnQtc3VibWlzc2lvbnMiXSwiZXhwIjozNjc3MzQ4MjE2LCJhdXRob3JpdGllcyI6WyJST0xFX1RSVVNURURfQ0xJRU5UIiwiUk9MRV9DTElFTlQiXSwianRpIjoiODdmNDRmYjYtZjY3Mi00YWQ1LTk2NzctM2NkMjQyZGNhZTkyIiwiY2xpZW50X2lkIjoidmNmSXJBMW92OEMwaTgyMUU4b3QifQ.PS0-SDcA7VSCzxiXTl5bZESX-FGaO8g_HtRIglFawXsSMpmcmkjwgWftBhQsqtl831W3Rk3ulESHFKzfWm1MFrriU42YQb-9ZcjLouOrxYUyY-Vll1rrPjGAb9t3c7CtkQEsZ2_NfQ4zmaZ6vLd9BtaIbHqv_AYOMQCpY8ht4hc`
+            },url:'http://services.innofactory.io/payment/payments'},
+            function (error, response, result) {
+            if (!error && response.statusCode == 200) {
+                var DataTransform = require("node-json-transform").DataTransform;
+                var app = require('../../server/server');
+                var map = {
+                    item: {
+                        id: "Data.PaymentId",
+                        category: "Data.Initiation.RemittanceInformation.Unstructured",
+                        location: "Data.Initiation.RemittanceInformation.Unstructured",
+                        date: "Data.CreationDateTime",
+                        amount:"Data.Initiation.InstructedAmount.Amount",
+                        product:"Data.Initiation.RemittanceInformation.Unstructured"
+                    },
+                    operate: [
+                    {   run: function(val) {    var arr=val.split("###"); console.log(arr);if(arr.length>0) return arr[0]; else return "";}, on: "category"  },
+                    {   run: function(val) {  var arr=val.split("###"); if(arr.length>1) return arr[1]; else return "";}, on: "location"  },
+                    {   run: function(val) {  var arr=val.split("###"); if(arr.length>2) return arr[2]; else return "";}, on: "product"  }
+                ]
+                };
+                var dataTransform = DataTransform(JSON.parse(result), map);
+                var transformedResult = dataTransform.transform();
+                var fs = require('fs');
+                fs.writeFile("test", JSON.stringify(transformedResult), function(err) {
+                    if(err) {
+                        return console.log(err);
+                    }
+
+                    console.log("The file was saved!");
+                }); 
+            }
+        });        
+
+        callback(null,"List of transaction imported");
     }; 
     
     BadHabit.remoteMethod(
@@ -103,6 +143,20 @@ module.exports = function(BadHabit) {
             ],
             returns: [
                 {arg: 'message', type: 'string'},
+          ]
+        }
+        
+    );  
+
+    BadHabit.remoteMethod(
+        'import', {
+            http:{
+                path:'/import',
+                verb:'get'                
+            },
+            description:'Load data from api sandbox',
+            returns: [
+                {arg: 'List of transactions', type: 'string'},
           ]
         }
         
