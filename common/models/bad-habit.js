153d84d4fa84f843d6d946ee1c90c2df4c7ee66a
Beaule <frederic.lebeau.mobile@gmail.com>
2018-06-27 08:04:05 +0200

    badhabits retrieved from sandbox (still ugly)

--- common/models/bad-habit.js
+++ common/models/bad-habit.js
@@ -10,11 +10,11 @@ module.exports = function(BadHabit) {
     /***********************************
      * Enable-disable API exposed methods
      ************************************/    
-    //BadHabit.disableRemoteMethodByName("create", false);
+    BadHabit.disableRemoteMethodByName("create", false);
     BadHabit.disableRemoteMethodByName("replaceOrCreate", false);
     BadHabit.disableRemoteMethodByName("patchOrCreate", false);
     BadHabit.disableRemoteMethodByName("exists", false);
-    //BadHabit.disableRemoteMethodByName("find", true);
+    BadHabit.disableRemoteMethodByName("find", true);
     BadHabit.disableRemoteMethodByName("findById", false);
     BadHabit.disableRemoteMethodByName("findOne", false);
     BadHabit.disableRemoteMethodByName("deleteById", false);
@@ -29,7 +29,7 @@ module.exports = function(BadHabit) {
     /***********************************
      * Extend methods
      ************************************/ 
-    //get habits overview
+    //get habits overview from local file
    BadHabit.analysis= function(product,startDate,callback){
        //calcul total-spends
        var filter={where: {date: {gt: startDate}}};
@@ -58,18 +58,12 @@ module.exports = function(BadHabit) {
                     }
                 }
             }
-            //if (maxFrequentLocationElement!=null)
-                callback(null,Math.round(totalBadHabits * 100) / 100,Math.round(totalSpends * 100) / 100,totalNumberOfBadHabits,maxFrequentLocationElement,maxFrequentLocationAmount);
+            callback(null,Math.round(totalBadHabits * 100) / 100,Math.round(totalSpends * 100) / 100,totalNumberOfBadHabits,maxFrequentLocationElement,maxFrequentLocationAmount);
         });      
     };
 
-    //confirm user want to be notify when badhabit happens
-    BadHabit.notify= function(product,callback){
-            callback(null,"Notifications activated for product: "+product);
-    }; 
-
-     //load Bad habits from API sandbox
-     BadHabit.import= function(callback){
+    //get habits overview from sandbox
+   BadHabit.analysisFromSandbox= function(product,startDate,callback){
         var request = require('request');
         request({
                 headers: { 'Authorization': `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsib2F1dGgyLXJlc291cmNlIl0sInNjb3BlIjpbInNlcnZlciIsInBheW1lbnRzIiwicHJvZmlsZXMiLCJhY2NvdW50cyIsInRyYW5zYWN0aW9ucyIsInBheW1lbnQtc3VibWlzc2lvbnMiXSwiZXhwIjozNjc3MzQ4MjE2LCJhdXRob3JpdGllcyI6WyJST0xFX1RSVVNURURfQ0xJRU5UIiwiUk9MRV9DTElFTlQiXSwianRpIjoiODdmNDRmYjYtZjY3Mi00YWQ1LTk2NzctM2NkMjQyZGNhZTkyIiwiY2xpZW50X2lkIjoidmNmSXJBMW92OEMwaTgyMUU4b3QifQ.PS0-SDcA7VSCzxiXTl5bZESX-FGaO8g_HtRIglFawXsSMpmcmkjwgWftBhQsqtl831W3Rk3ulESHFKzfWm1MFrriU42YQb-9ZcjLouOrxYUyY-Vll1rrPjGAb9t3c7CtkQEsZ2_NfQ4zmaZ6vLd9BtaIbHqv_AYOMQCpY8ht4hc`
@@ -88,29 +82,50 @@ module.exports = function(BadHabit) {
                         product:"Data.Initiation.RemittanceInformation.Unstructured"
                     },
                     operate: [
-                    {   run: function(val) {    var arr=val.split("###"); console.log(arr);if(arr.length>0) return arr[0]; else return "";}, on: "category"  },
+                    {   run: function(val) {    var arr=val.split("###");if(arr.length>0) return arr[0]; else return "";}, on: "category"  },
                     {   run: function(val) {  var arr=val.split("###"); if(arr.length>1) return arr[1]; else return "";}, on: "location"  },
                     {   run: function(val) {  var arr=val.split("###"); if(arr.length>2) return arr[2]; else return "";}, on: "product"  }
                 ]
                 };
                 var dataTransform = DataTransform(JSON.parse(result), map);
-                var transformedResult = dataTransform.transform();
-                var fs = require('fs');
-                fs.writeFile("test", JSON.stringify(transformedResult), function(err) {
-                    if(err) {
-                        return console.log(err);
+                var badHabits = dataTransform.transform();
+                var totalBadHabits=0;
+                var totalSpends=0;
+                var frequentLocationMap = {};
+                var frequentLocationCurrentElement = null;
+                var maxFrequentLocationAmount = 0;
+                var maxFrequentLocationElement = null;
+                var totalNumberOfBadHabits=0;
+                for (var key in badHabits) {
+                    totalSpends=totalSpends+Math.abs(badHabits[key].amount);
+                    if(badHabits[key].product==product){
+                        totalBadHabits=totalBadHabits+Math.abs(badHabits[key].amount);
+                        totalNumberOfBadHabits=totalNumberOfBadHabits+1;
+                        frequentLocationCurrentElement = badHabits[key].location;
+                        if(frequentLocationMap[frequentLocationCurrentElement] == null)
+                            frequentLocationMap[frequentLocationCurrentElement] = 1;
+                        else
+                            frequentLocationMap[frequentLocationCurrentElement]++;  
+                        if(frequentLocationMap[frequentLocationCurrentElement] > maxFrequentLocationAmount){
+                            maxFrequentLocationElement = frequentLocationCurrentElement;
+                            maxFrequentLocationAmount = frequentLocationMap[frequentLocationCurrentElement];
+                        }
                     }
+                }
+                callback(null,Math.round(totalBadHabits * 100) / 100,Math.round(totalSpends * 100) / 100,totalNumberOfBadHabits,maxFrequentLocationElement,maxFrequentLocationAmount);
 
-                    console.log("The file was saved!");
-                }); 
             }
-        });        
+        });   
+    };
 
-        callback(null,"List of transaction imported");
+    //confirm user want to be notify when badhabit happens
+    BadHabit.notify= function(product,callback){
+            callback(null,"Notifications activated for product: "+product);
     }; 
+
     
     BadHabit.remoteMethod(
-        'analysis', {
+        'analysisFromSandbox', {
             http:{
                 path:'/analysis',
                 verb:'get'                
@@ -130,6 +145,8 @@ module.exports = function(BadHabit) {
         }
     );  
 
+    
+
 
     BadHabit.remoteMethod(
         'notify', {
@@ -146,19 +163,5 @@ module.exports = function(BadHabit) {
           ]
         }
         
-    );  
-
-    BadHabit.remoteMethod(
-        'import', {
-            http:{
-                path:'/import',
-                verb:'get'                
-            },
-            description:'Load data from api sandbox',
-            returns: [
-                {arg: 'List of transactions', type: 'string'},
-          ]
-        }
-        
-    );  
+    ); 
 };
