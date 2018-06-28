b66f4f3cb07d98e7522af804f21c8c8e21fa1b27
Beaule <frederic.lebeau.mobile@gmail.com>
2018-06-28 13:07:23 +0200

    back to sandboxed data

--- common/models/bad-habit.js
+++ common/models/bad-habit.js
@@ -125,7 +125,7 @@ module.exports = function(BadHabit) {
 
     
     BadHabit.remoteMethod(
-        'analysis', {
+        'analysisFromSandbox', {
             http:{
                 path:'/analysis',
                 verb:'get'                
